3a2e67c3e926c4e8d62c9013fd819aa694b5ab68
Colton Acosta <cacost12@asu.edu>
2023-03-15 19:00:22 -0700

    implemented ping code

---
+++ app/ping/rev1/main.c
@@ -0,0 +1,364 @@
+/*******************************************************************************
+*
+* FILE: 
+*       main.c
+*
+* DESCRIPTION: 
+*		Pings another XBee module and indicates to PC when a ping code has 
+*       received. Used to perform range testing and bidirectional communcation
+*       testing.	
+*
+*******************************************************************************/
+
+
+/*------------------------------------------------------------------------------
+Standard Includes                                                                     
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+Project Includes                                                                     
+------------------------------------------------------------------------------*/
+
+/* Pin definitions, main prototypes, error codes */
+#include "main.h"
+#include "sdr_pin_defines_A0005.h"
+#include "sdr_error.h"
+
+/* SDR Modules */
+#include "commands.h"
+#include "led.h"
+#include "usb.h"
+#include "wireless.h"
+
+
+/*------------------------------------------------------------------------------
+Global Variables                                                                  
+------------------------------------------------------------------------------*/
+
+/* MCU Peripheral handles */
+UART_HandleTypeDef huart4; /* Xbee UART */
+UART_HandleTypeDef huart1; /* USB UART  */
+
+
+/*------------------------------------------------------------------------------
+Typedefs                                                                  
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+Function prototypes                                                          
+------------------------------------------------------------------------------*/
+void        SystemClock_Config( void );
+static void GPIO_Init         ( void );
+static void XBee_UART_Init    ( void );
+static void USB_UART_Init     ( void );
+
+
+/*------------------------------------------------------------------------------
+Application entry point                                                      
+------------------------------------------------------------------------------*/
+int main
+	(
+	void
+	)
+{
+/*------------------------------------------------------------------------------
+ Local Variables 
+------------------------------------------------------------------------------*/
+uint8_t    rx_byte;     /* Byte recieved from Wireless module */
+USB_STATUS usb_status;  /* Status of USB module               */
+RF_STATUS  rf_status;   /* Status of wireless module          */
+
+
+/*------------------------------------------------------------------------------
+ Initializations                                                             
+------------------------------------------------------------------------------*/
+rx_byte = 0;
+
+
+/*------------------------------------------------------------------------------
+ MCU Initialization                                                             
+------------------------------------------------------------------------------*/
+HAL_Init          ();   /* CMSIS HAL */
+SystemClock_Config();   /* SysClock  */
+GPIO_Init         ();   /* GPIO Pins */
+XBee_UART_Init    ();   /* XBee      */
+USB_UART_Init     ();   /* USB       */
+
+
+/*------------------------------------------------------------------------------
+ Event Loop                                                                  
+------------------------------------------------------------------------------*/
+while (1)
+	{
+	/* Poll the wireless module for incoming data */
+	rf_status = rf_xbee_receive_byte( &rx_byte ); 
+
+	/* Received byte */
+	if ( rf_status != RF_TIMEOUT )
+		{
+		/* Correct byte? */
+		if ( rx_byte != PING_OP )
+			{
+			Error_Handler( ERROR_RF_UNRECOGNIZED_PING );
+			}
+
+		/* Send a ping to the other wireless module */
+		rf_xbee_transmit_byte( PING_OP );
+
+		/* Tell PC a ping has been received */
+	    usb_status = usb_transmit( &rx_byte         , 
+                                   sizeof( uint8_t ),
+                                   HAL_DEFAULT_TIMEOUT  );
+		if ( usb_status != USB_OK )
+			{
+			Error_Handler( ERROR_USB_UART_ERROR );
+			}
+		} /* if ( rf_status != RF_TIMEOUT ) */
+	}
+} /* main */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+*       SystemClock_Config                                                     *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Initializes the microcontroller clock. Enables peripheral clocks and   *
+*       sets prescalers                                                        *
+*                                                                              *
+*******************************************************************************/
+void SystemClock_Config
+	(
+	void
+	)
+{
+/* Initialization Structs */
+RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+/* Supply configuration update enable */
+HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+
+/* Configure the main internal regulator output voltage */
+__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) 
+	{
+	/* Wait for PWR_FLAG_VOSRDY flag */
+	}
+
+/* Initializes the RCC Oscillators according to the specified parameters
+   in the RCC_OscInitTypeDef structure. */
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+RCC_OscInitStruct.PLL.PLLM       = 2;
+RCC_OscInitStruct.PLL.PLLN       = 80;
+RCC_OscInitStruct.PLL.PLLP       = 2;
+RCC_OscInitStruct.PLL.PLLQ       = 2;
+RCC_OscInitStruct.PLL.PLLR       = 2;
+RCC_OscInitStruct.PLL.PLLRGE     = RCC_PLL1VCIRANGE_3;
+RCC_OscInitStruct.PLL.PLLVCOSEL  = RCC_PLL1VCOWIDE;
+RCC_OscInitStruct.PLL.PLLFRACN   = 0;
+if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+	Error_Handler( ERROR_SYSCLOCK_CONFIG_ERROR );
+	}
+
+/* Initializes the CPU, AHB and APB buses clocks */
+RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK    |
+	                               RCC_CLOCKTYPE_SYSCLK  |
+						           RCC_CLOCKTYPE_PCLK1   |
+                                   RCC_CLOCKTYPE_PCLK2   |
+						           RCC_CLOCKTYPE_D3PCLK1 |
+		                           RCC_CLOCKTYPE_D1PCLK1;
+RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+RCC_ClkInitStruct.SYSCLKDivider  = RCC_SYSCLK_DIV1;
+RCC_ClkInitStruct.AHBCLKDivider  = RCC_HCLK_DIV2;
+RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+/* Set the configuration */
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+	{
+	Error_Handler( ERROR_SYSCLOCK_CONFIG_ERROR );
+	}
+} /* SystemClock_Config */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+*       XBee_UART_Init                                                         *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*		Initializes the UART peripheral attached to the XBee module            *
+*                                                                              *
+*******************************************************************************/
+static void XBee_UART_Init
+	(
+	void
+	)
+{
+/* Fill configuration struct with UART settings */
+huart4.Instance                    = UART4;
+huart4.Init.BaudRate               = 9600;
+huart4.Init.WordLength             = UART_WORDLENGTH_8B;
+huart4.Init.StopBits               = UART_STOPBITS_1;
+huart4.Init.Parity                 = UART_PARITY_NONE;
+huart4.Init.Mode                   = UART_MODE_TX_RX;
+huart4.Init.HwFlowCtl              = UART_HWCONTROL_NONE;
+huart4.Init.OverSampling           = UART_OVERSAMPLING_16;
+huart4.Init.OneBitSampling         = UART_ONE_BIT_SAMPLE_DISABLE;
+huart4.Init.ClockPrescaler         = UART_PRESCALER_DIV1;
+huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+/* Apply configuration */
+if (HAL_UART_Init(&huart4) != HAL_OK)
+	{
+	Error_Handler( ERROR_XBEE_UART_CONFIG_ERROR );
+	}
+if (HAL_UARTEx_SetTxFifoThreshold(&huart4, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+	{
+	Error_Handler( ERROR_XBEE_UART_CONFIG_ERROR );
+	}
+if (HAL_UARTEx_SetRxFifoThreshold(&huart4, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+	{
+	Error_Handler( ERROR_XBEE_UART_CONFIG_ERROR );
+	}
+if (HAL_UARTEx_DisableFifoMode(&huart4) != HAL_OK)
+	{
+	Error_Handler( ERROR_XBEE_UART_CONFIG_ERROR );
+	}
+
+} /* XBee_UART_Init() */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+*       USB_UART_Init                                                          *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*		Initializes the UART peripheral attached to the USB port               *
+*                                                                              *
+*******************************************************************************/
+static void USB_UART_Init 
+	(
+	void
+	)
+{
+
+/* Fill configuration struct with UART settings */
+huart1.Instance                    = USART1;
+huart1.Init.BaudRate               = 9600;
+huart1.Init.WordLength             = UART_WORDLENGTH_8B;
+huart1.Init.StopBits               = UART_STOPBITS_1;
+huart1.Init.Parity                 = UART_PARITY_NONE;
+huart1.Init.Mode                   = UART_MODE_TX_RX;
+huart1.Init.HwFlowCtl              = UART_HWCONTROL_NONE;
+huart1.Init.OverSampling           = UART_OVERSAMPLING_16;
+huart1.Init.OneBitSampling         = UART_ONE_BIT_SAMPLE_DISABLE;
+huart1.Init.ClockPrescaler         = UART_PRESCALER_DIV1;
+huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+/* Apply configuration */
+if ( HAL_UART_Init( &huart1 ) != HAL_OK )
+	{
+	Error_Handler( ERROR_USB_UART_INIT_ERROR );
+	}
+if ( HAL_UARTEx_SetTxFifoThreshold( &huart1, UART_TXFIFO_THRESHOLD_1_8 ) != HAL_OK )
+	{
+	Error_Handler( ERROR_USB_UART_INIT_ERROR );
+	}
+if ( HAL_UARTEx_SetRxFifoThreshold( &huart1, UART_RXFIFO_THRESHOLD_1_8 ) != HAL_OK )
+	{
+	Error_Handler( ERROR_USB_UART_INIT_ERROR );
+	}
+if ( HAL_UARTEx_DisableFifoMode( &huart1 ) != HAL_OK )
+	{
+	Error_Handler( ERROR_USB_UART_INIT_ERROR );
+	}
+
+} /*  USB_UART_Init() */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   *
+*       GPIO_Init                                                              * 
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+*       Initializes all GPIO pins and sets alternate functions                 *
+*                                                                              *
+*******************************************************************************/
+static void GPIO_Init
+	(
+	void
+	)
+{
+/* GPIO Initialization Struct */
+GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+/* GPIO Ports Clock Enable */
+__HAL_RCC_GPIOH_CLK_ENABLE();
+__HAL_RCC_GPIOA_CLK_ENABLE();
+__HAL_RCC_GPIOE_CLK_ENABLE();
+
+/*--------------------------- LED MCU PINS -----------------------------------*/
+
+/*Configure GPIO pin Output Level */
+HAL_GPIO_WritePin( STATUS_GPIO_PORT, 
+			       STATUS_R_PIN      |
+		           STATUS_G_PIN      |
+                   STATUS_B_PIN    , 
+				   GPIO_PIN_RESET );
+
+/*Configure GPIO pins : PE10 PE11 PE12 */
+GPIO_InitStruct.Pin   = STATUS_R_PIN |
+                        STATUS_G_PIN |
+                        STATUS_B_PIN;
+GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+GPIO_InitStruct.Pull  = GPIO_NOPULL;
+GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+} /* GPIO_Init */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   *
+*       Error_Handler                                                          * 
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		This function is executed in case of error occurrence                  *
+*                                                                              *
+*******************************************************************************/
+void Error_Handler
+	(
+	volatile ERROR_CODE error_code
+	)
+{
+/* User can add his own implementation to report the HAL error return state */
+__disable_irq();
+
+/* LED indication */
+led_error_assert();
+
+/* Idle */
+while (1)
+	{
+	}
+} /* Error_Handler */
+
+
+/*******************************************************************************
+* END OF FILE                                                                  *
+*******************************************************************************/
---
+++ app/ping/rev1/main.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+*
+* FILE: 
+*       main.h
+*
+* DESCRIPTION: 
+*		Pings another XBee module and indicates to PC when a ping code has 
+*       received. Used to perform range testing and bidirectional communcation
+*       testing.	
+*
+*******************************************************************************/
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*------------------------------------------------------------------------------
+Standard Includes                                                                     
+------------------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+#include "sdr_error.h"
+
+
+/*------------------------------------------------------------------------------
+Standard Includes                                                                     
+------------------------------------------------------------------------------*/
+
+/* General HAL related macros */
+#define HAL_DEFAULT_TIMEOUT		( 1 )
+
+/*------------------------------------------------------------------------------
+Project Includes                                                                     
+------------------------------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+
+/* Handles miscellaneous errors */
+void Error_Handler
+    ( 
+    volatile ERROR_CODE error_code 
+    );
+
+/* Private defines -----------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
