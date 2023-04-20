2fe3d9dca1f4087245b3fe6283275be223e5b620
Colton Acosta <cacost12@asu.edu>
2023-04-19 18:21:33 -0700

    added terminal code and added ping and connect commands

--- app/terminal/rev1/main.c
+++ app/terminal/rev1/main.c
@@ -55,23 +55,25 @@ int main
 /*------------------------------------------------------------------------------
  Local Variables 
 ------------------------------------------------------------------------------*/
-uint8_t    rx_byte;            /* Byte recieved from Wireless module  */
+uint8_t       command;          /* SDEC command                               */
+uint8_t       board_code;       /* Ping code of connected board               */
 #ifdef USE_RS485
 	RS485_STATUS rs485_status; /* RS485 return codes                  */
 #else
 	RF_STATUS    rf_status;    /* Status of wireless module           */
 #endif
 USB_STATUS usb_status;         /* Status of USB module                */
-uint32_t   time;               /* Time for initial ping timeout       */
-uint32_t   start_time;         /* Starting time for timeout counter   */
-uint32_t   timeout_cnt;        /* Number of times ping has timeed out */
 
 
 /*------------------------------------------------------------------------------
  Initializations                                                             
 ------------------------------------------------------------------------------*/
-rx_byte     = 0;
-timeout_cnt = 0;
+#ifdef USE_RS485
+	rs485_status = RS485_OK;
+#else
+	rf_status    = RF_OK;
+#endif
+usb_status = USB_OK;
 
 
 /*------------------------------------------------------------------------------
@@ -88,8 +90,6 @@ RS485_UART_Init   ();   /* RS485     */
 /*------------------------------------------------------------------------------
  Initial Setup 
 ------------------------------------------------------------------------------*/
-start_time = HAL_GetTick();
-time       = HAL_GetTick() - start_time;
 
 /* Successfuly initialization LED indication */
 led_set_color( LED_GREEN );
@@ -98,135 +98,97 @@ led_set_color( LED_GREEN );
 /*------------------------------------------------------------------------------
  Event Loop                                                                  
 ------------------------------------------------------------------------------*/
-#ifdef USE_RS485
-	while (1)
+while ( 1 )
+	{
+	/* Read data from UART reciever */
+    usb_status = usb_receive( &command, sizeof( command ), HAL_DEFAULT_TIMEOUT );
+
+		/* Parse command input if HAL_UART_Receive doesn't timeout */
+	if ( usb_status != USB_TIMEOUT )
 		{
-		/* Update timeout counter */
-		time = HAL_GetTick() - start_time; 
-
-		/* Poll the wireless module for incoming data */
-		rs485_status = rs485_receive_byte( &rx_byte ); 
-
-		/* Received byte */
-		if ( ( rs485_status != RS485_TIMEOUT ) && ( rx_byte != 0 ) )
+		switch( command )
 			{
-			led_set_color( LED_GREEN );
-			HAL_Delay( 100 );
-
-			/* Correct byte? */
-			if ( rx_byte != PING_CODE )
+			/*-----------------------------------------------------------------
+			 PING Command	
+			------------------------------------------------------------------*/
+			case PING_OP:
 				{
-				Error_Handler( ERROR_RF_UNRECOGNIZED_PING );
-				}
+				/* Send Command to engine controller */
+				rs485_status = rs485_transmit( &command         , 
+				                               sizeof( command ), 
+											   RS485_DEFAULT_TIMEOUT );
+				if ( rs485_status != RS485_OK )
+					{
+					Error_Handler( ERROR_RS485_UART_ERROR );
+					}
 
-			/* Tell PC a ping has been received */
-			usb_status = usb_transmit( &rx_byte         , 
-									sizeof( uint8_t ),
-									2 );
-			if ( usb_status != USB_OK )
+				/* Receive Engine controller ping response */
+				rs485_status = rs485_receive( &board_code         , 
+				                              sizeof( board_code ), 
+											  RS485_DEFAULT_TIMEOUT );
+				if ( rs485_status != RS485_OK )
+					{
+					Error_Handler( ERROR_RS485_UART_ERROR );
+					}
+				
+				/* Transmit Ping response back to PC */
+				usb_status = usb_transmit( &board_code, 
+				                           sizeof( board_code ), 
+										   RS485_DEFAULT_TIMEOUT );
+				if ( usb_status != USB_OK ) 
+					{
+					Error_Handler( ERROR_USB_UART_ERROR );
+					}
+				
+				break;
+				} /* PING_OP */
+
+			/*-----------------------------------------------------------------
+			 CONNECT Command	
+			------------------------------------------------------------------*/
+			case CONNECT_OP:
 				{
-				Error_Handler( ERROR_USB_UART_ERROR );
-				}
+				/* Send Command to engine controller */
+				rs485_status = rs485_transmit( &command         , 
+				                               sizeof( command ), 
+											   RS485_DEFAULT_TIMEOUT );
+				if ( rs485_status != RS485_OK )
+					{
+					Error_Handler( ERROR_RS485_UART_ERROR );
+					}
 
-			/* Send a ping to the other wireless module */
-			rs485_transmit_byte( PING_CODE );
+				/* Receive Engine controller ping response */
+				rs485_status = rs485_receive( &board_code         , 
+				                              sizeof( board_code ), 
+											  RS485_DEFAULT_TIMEOUT );
+				if ( rs485_status != RS485_OK )
+					{
+					Error_Handler( ERROR_RS485_UART_ERROR );
+					}
+				
+				/* Transmit Ping response back to PC */
+				usb_status = usb_transmit( &board_code, 
+				                           sizeof( board_code ), 
+										   RS485_DEFAULT_TIMEOUT );
+				if ( usb_status != USB_OK ) 
+					{
+					Error_Handler( ERROR_USB_UART_ERROR );
+					}
+				break;
+				} /* CONNECT_OP */
 
-			} /* if ( rs485_status != RS485_TIMEOUT ) */
-		else
-			{
-			led_set_color( LED_YELLOW );
-			}
-
-		/* Send out a ping if a ping hasn't been receieved recently */
-		if ( time >= PING_TIMEOUT )
-			{
-			rs485_status = rs485_transmit_byte( PING_CODE );
-			if ( rs485_status != RS485_OK )
+			/*-----------------------------------------------------------------
+			 Unrecognized Command 
+			------------------------------------------------------------------*/
+			default:
 				{
-				Error_Handler( ERROR_RF_ERROR );
-				}
-			start_time = HAL_GetTick();
-			timeout_cnt++;
-			}
-		
-
-		/* Error out if timeout count is too large */
-		/*
-		if ( timeout_cnt > 10 )
-			{
-			while ( 1 )
-				{
-				led_set_color( LED_BLUE );
-				}
-			}
-		*/
-
-		}
-#else
-	while (1)
-		{
-		/* Update timeout counter */
-		time = HAL_GetTick() - start_time; 
-
-		/* Poll the wireless module for incoming data */
-		rf_status = rf_xbee_receive_byte( &rx_byte ); 
-
-		/* Received byte */
-		if ( rf_status != RF_TIMEOUT )
-			{
-			led_set_color( LED_GREEN );
-			/* Correct byte? */
-			/*
-			if ( rx_byte != PING_CODE )
-				{
-				Error_Handler( ERROR_RF_UNRECOGNIZED_PING );
-				}
-			*/
-
-			/* Send a ping to the other wireless module */
-			rf_xbee_transmit_byte( PING_CODE );
-
-			/* Tell PC a ping has been received */
-			/*
-			usb_status = usb_transmit( &rx_byte         , 
-									sizeof( uint8_t ),
-									2 );
-			if ( usb_status != USB_OK )
-				{
-				Error_Handler( ERROR_USB_UART_ERROR );
-				}
-			*/
-			} /* if ( rf_status != RF_TIMEOUT ) */
-		else
-			{
-			led_set_color( LED_YELLOW );
-			}
-
-		/* Send out a ping if a ping hasn't been receieved recently */
-		if ( time >= PING_TIMEOUT )
-			{
-			rf_status = rf_xbee_transmit_byte( PING_CODE );
-			if ( rf_status != RF_OK )
-				{
-				Error_Handler( ERROR_RF_ERROR );
-				}
-			start_time = HAL_GetTick();
-			timeout_cnt++;
-			}
-
-		/* Error out if timeout count is too large */
-		/*
-		if ( timeout_cnt > 10 )
-			{
-			while ( 1 )
-				{
-				led_set_color( LED_BLUE );
-				}
-			}
-		*/
-
-		}
-#endif
+				/* Unsupported command code flash the red LED */
+				//Error_Handler();
+				} /* default */
+			} 
+		} 
+	
+	}
 } /* main */
 
 
