d3d772730d11ef6de278b9cf6fe761923be83100
ETSsound <147210601+ETSsound@users.noreply.github.com>
2025-11-18 14:50:08 -0700

    Base firmware app for receiver

--- init/init.c
+++ init/init.c
@@ -162,41 +162,41 @@ if ( HAL_UARTEx_DisableFifoMode( &huart1 ) != HAL_OK )
 *		Initializes the SPI peripheral for Long Range RFM95 Module               *
 *                                                                              *
 *******************************************************************************/
-void LORA_SPI_Init
-	(
-		void
-	)
-{
+// void LORA_SPI_Init
+// 	(
+// 		void
+// 	)
+// {
 
-/* Fill configuration struct with Lora settings */
-hspi2.Instance = SPI2;
-hspi2.Init.Mode = SPI_MODE_MASTER;
-hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
-hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-hspi2.Init.NSS = SPI_NSS_SOFT;
-hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
-hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-hspi2.Init.CRCPolynomial = 0x0;
-hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-hspi2.Init.NSSPolarity = SPI_NSS_POLARITY_LOW;
-hspi2.Init.FifoThreshold = SPI_FIFO_THRESHOLD_01DATA;
-hspi2.Init.TxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
-hspi2.Init.RxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
-hspi2.Init.MasterSSIdleness = SPI_MASTER_SS_IDLENESS_00CYCLE;
-hspi2.Init.MasterInterDataIdleness = SPI_MASTER_INTERDATA_IDLENESS_00CYCLE;
-hspi2.Init.MasterReceiverAutoSusp = SPI_MASTER_RX_AUTOSUSP_DISABLE;
-hspi2.Init.MasterKeepIOState = SPI_MASTER_KEEP_IO_STATE_DISABLE;
-hspi2.Init.IOSwap = SPI_IO_SWAP_DISABLE;
-if (HAL_SPI_Init(&hspi2) != HAL_OK)
-{
-Error_Handler(ERROR_LORA_SPI_INIT_ERROR);
-}
+// /* Fill configuration struct with Lora settings */
+// hspi2.Instance = SPI2;
+// hspi2.Init.Mode = SPI_MODE_MASTER;
+// hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+// hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+// hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+// hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+// hspi2.Init.NSS = SPI_NSS_SOFT;
+// hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+// hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+// hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+// hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+// hspi2.Init.CRCPolynomial = 0x0;
+// hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+// hspi2.Init.NSSPolarity = SPI_NSS_POLARITY_LOW;
+// hspi2.Init.FifoThreshold = SPI_FIFO_THRESHOLD_01DATA;
+// hspi2.Init.TxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
+// hspi2.Init.RxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
+// hspi2.Init.MasterSSIdleness = SPI_MASTER_SS_IDLENESS_00CYCLE;
+// hspi2.Init.MasterInterDataIdleness = SPI_MASTER_INTERDATA_IDLENESS_00CYCLE;
+// hspi2.Init.MasterReceiverAutoSusp = SPI_MASTER_RX_AUTOSUSP_DISABLE;
+// hspi2.Init.MasterKeepIOState = SPI_MASTER_KEEP_IO_STATE_DISABLE;
+// hspi2.Init.IOSwap = SPI_IO_SWAP_DISABLE;
+// if (HAL_SPI_Init(&hspi2) != HAL_OK)
+// {
+// Error_Handler(ERROR_LORA_SPI_INIT_ERROR);
+// }
 
-}
+// }
 
 /*******************************************************************************
 *                                                                              *
@@ -231,39 +231,39 @@ HAL_GPIO_WritePin( STATUS_GPIO_PORT,
                    STATUS_B_PIN    , 
 				   GPIO_PIN_SET );
 
-HAL_GPIO_WritePin( GPIOC, 
-				   LORA_IO5_PIN	|
-				   LORA_IO2_PIN, 
-				   GPIO_PIN_RESET );
+// HAL_GPIO_WritePin( GPIOC, 
+// 				   LORA_IO5_PIN	|
+// 				   LORA_IO2_PIN, 
+// 				   GPIO_PIN_RESET );
 
-HAL_GPIO_WritePin( GPIOA, 
-				   LORA_RST_PIN |
-				   LORA_NSS_PIN  | 
-				   LORA_IO3_PIN |
-				   LORA_IO4_PIN |
-				   LORA_IO0_PIN |
-				   LORA_IO1_PIN, 
-				   GPIO_PIN_RESET );
+// HAL_GPIO_WritePin( GPIOA, 
+// 				   LORA_RST_PIN |
+// 				   LORA_NSS_PIN  | 
+// 				   LORA_IO3_PIN |
+// 				   LORA_IO4_PIN |
+// 				   LORA_IO0_PIN |
+// 				   LORA_IO1_PIN, 
+// 				   GPIO_PIN_RESET );
 
 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12, GPIO_PIN_RESET);
 
-GPIO_InitStruct.Pin = LORA_IO5_PIN |
-					  LORA_IO2_PIN;
-GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-GPIO_InitStruct.Pull = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+// GPIO_InitStruct.Pin = LORA_IO5_PIN |
+// 					  LORA_IO2_PIN;
+// GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+// GPIO_InitStruct.Pull = GPIO_NOPULL;
+// GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+// HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-GPIO_InitStruct.Pin = LORA_RST_PIN |
-					  LORA_NSS_PIN  |
-					  LORA_IO3_PIN |
-					  LORA_IO4_PIN |
-					  LORA_IO0_PIN |
-					  LORA_IO1_PIN;
-GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-GPIO_InitStruct.Pull = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+// GPIO_InitStruct.Pin = LORA_RST_PIN |
+// 					  LORA_NSS_PIN  |
+// 					  LORA_IO3_PIN |
+// 					  LORA_IO4_PIN |
+// 					  LORA_IO0_PIN |
+// 					  LORA_IO1_PIN;
+// GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+// GPIO_InitStruct.Pull = GPIO_NOPULL;
+// GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+// HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 /*Configure GPIO pins : PE10 PE11 PE12 */
 GPIO_InitStruct.Pin   = STATUS_R_PIN |
